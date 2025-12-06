572f7d744bb055f9933a5a5e3fe1ed40c15252ea
Vesco Claudio <claudio.vesco@gmail.com>
2025-12-06 10:35:13 +0100

    day06

---
+++ days/day06/rs/src/lib.rs
@@ -0,0 +1,132 @@
+#![no_std]
+
+/// # Panics
+#[must_use]
+pub fn part_1(data: &str) -> u64 {
+    let table = data.as_bytes();
+    let columns = table
+        .iter()
+        .position(|&tile| tile == b'\n')
+        .expect("invalid input");
+    let rows = (table.len() - 1) / columns;
+
+    let parse = |slice: &[u8]| {
+        let mut exp = 1;
+        let mut num = 0;
+        for &digit in slice.iter().rev() {
+            match digit {
+                c if c.is_ascii_digit() => {
+                    num += u64::from(c - b'0') * exp;
+                    exp *= 10;
+                }
+                b' ' => {}
+                c => unreachable!("'{slice:?}': '{}'", c as char),
+            }
+        }
+        num
+    };
+
+    let ops = &table[(rows - 1) * (columns + 1)..];
+
+    let mut total = 0;
+    let mut column = 0;
+    while column < columns {
+        let op = ops[column];
+        let end_column = ops[column + 1..]
+            .iter()
+            .position(|&tile| tile == b'*' || tile == b'+')
+            .map_or(columns, |position| position + column);
+
+        total += match op {
+            b'+' => (0..rows - 1)
+                .map(|row| {
+                    let start = row * (columns + 1);
+                    parse(&table[start + column..start + end_column])
+                })
+                .sum::<u64>(),
+            b'*' => (0..rows - 1)
+                .map(|row| {
+                    let start = row * (columns + 1);
+                    parse(&table[start + column..start + end_column])
+                })
+                .product::<u64>(),
+            c => unreachable!("{column}: '{}'", c as char),
+        };
+
+        column = end_column + 1;
+    }
+
+    total
+}
+
+/// # Panics
+#[must_use]
+pub fn part_2(data: &str) -> u64 {
+    let table = data.as_bytes();
+    let columns = table
+        .iter()
+        .position(|&tile| tile == b'\n')
+        .expect("invalid input");
+    let rows = (table.len() - 1) / columns;
+
+    let parse = |column| {
+        let mut exp = 1;
+        let mut num = 0;
+        for row in (0..rows - 1).rev() {
+            match table[row * (columns + 1) + column] {
+                c if c.is_ascii_digit() => {
+                    num += u64::from(c - b'0') * exp;
+                    exp *= 10;
+                }
+                b' ' => {}
+                c => unreachable!("({row}, {column}): '{}'", c as char),
+            }
+        }
+        num
+    };
+
+    let ops = &table[(rows - 1) * (columns + 1)..];
+
+    let mut total = 0;
+    let mut column = 0;
+    while column < columns {
+        let op = ops[column];
+        let end_column = ops[column + 1..]
+            .iter()
+            .position(|&tile| tile == b'*' || tile == b'+')
+            .map_or(columns, |position| position + column);
+
+        total += match op {
+            b'+' => (column..end_column).map(parse).sum::<u64>(),
+            b'*' => (column..end_column).map(parse).product::<u64>(),
+            c => unreachable!("{column}: '{}'", c as char),
+        };
+
+        column = end_column + 1;
+    }
+
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = r"123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+";
+
+    #[test]
+    #[allow(clippy::unreadable_literal)]
+    fn test_part_1() {
+        assert_eq!(part_1(INPUT), 4277556);
+    }
+
+    #[test]
+    #[allow(clippy::unreadable_literal)]
+    fn test_part_2() {
+        assert_eq!(part_2(INPUT), 3263827);
+    }
+}
